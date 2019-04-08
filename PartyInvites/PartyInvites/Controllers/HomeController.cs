8f2cd96b991b2312cb5eab7c1f9668e7de668242
Nikman <nur.nikman@gmail.com>
2019-04-08 11:42:26 +0800

    partyinvites

---
+++ PartyInvites/PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PartyInvites.Models;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+       /* public string Index()
+        {
+            return "Hello World";
+        }
+        */
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {   
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                //TODO: Email response to the party organizer  
+                return View("Thanks", guestResponse);
+            } else
+            {
+                //There is a validation error
+                return View();
+            }
+        }
+
+    }
+}
